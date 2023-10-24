c5c34151e695f4e086cfc8ab05e0d9d8fbac447d
Mathew Jacob <mjacob1002@gmail.com>
2023-10-23 21:16:13 -0500

    Hopefully works\n

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,11 @@
 package main;
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	pb "github.com/mjacob1002/425-MP3/pkg/gen_proto"
-	"google.golang.org/protobuf/proto"
 	membership "github.com/mjacob1002/425-MP3/pkg/membership"
 	"strconv"
 )
@@ -39,36 +38,25 @@ func main(){
 	InitializeLogger();
 	membership.InitializeMembership(); // setup the listening port	
 	// establishing the writing connection
-	responses := int(0);
+	go membership.ListenForHeartbeats();
 	if introducer != "" {
 		membership.SendHeartbeatMessage(membership.MakeHeartbeat(), introducer)
 	}
-	for responses < 1024 {
-		buffer := make([]byte , 4096);
-		log.Println("waiting for message")
-		n, addr , err := membership.MembershipConnection.ReadFromUDP(buffer)
-		if err != nil {
-			fmt.Println(err);
-			return
-		}
-		log.Println("received a message! has ", n , " bytes from ", addr);
-		var received_heartbeat = pb.HeartbeatMessage{}
-		err = proto.Unmarshal(buffer[0:n], &received_heartbeat);
-		if err != nil {
-			fmt.Println(err);
-			return;
-		}
-		responses++;
-		// merge the tables
-		membership.MergeTables(*(received_heartbeat.Table));
-		new_dst := (received_heartbeat.Host) + ":" + (received_heartbeat.Port)
-		if (responses % 50) == 0{
+	go membership.PingHeartbeats();
+	go membership.PruneTable();
+	for {
+		 reader := bufio.NewReader(os.Stdin);
+		 fmt.Printf("Type a command here\n");
+		 text, _ := reader.ReadString('\n')
+		 if text == "get members\n"{
+			membership.MembershipMutex.Lock();
+			for _, entry := range(membership.MembershipList){
+					fmt.Printf("%s\n", entry.String());
+			}
+			membership.MembershipMutex.Unlock()
+		 } else {
+			fmt.Println("Didn't recognize the following command: ", text);
+		 }
 
-			actual_string := received_heartbeat.String()
-			log.Printf("Received %s from a node\n", actual_string);
-			log.Println("New table: ", membership.MembershipList)
-		}
-		membership.SendHeartbeatMessage(membership.MakeHeartbeat(), new_dst);
 	}
-	log.Println("finished ping pong");
 }
--- pkg/membership/membership.go
+++ pkg/membership/membership.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	pb "github.com/mjacob1002/425-MP3/pkg/gen_proto"
 	"google.golang.org/protobuf/proto"
+	rand "math/rand"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -15,7 +17,11 @@ var MEMBERSHIP_ID string;
 var MEMBERSHIP_PORT string;
 var LogicalNode int64;
 var MembershipConnection *net.UDPConn;
+var MembershipMutex sync.Mutex;
 var MembershipList = make(map[string]pb.TableEntry);
+var MEMBERSHIP_TIMEOUT int64;
+var TIMEOUT int64;
+var TIMEOUT_CLEANUP int64;
 
 
 func SetupMembershipPort(){
@@ -35,6 +41,8 @@ func SetupMembershipPort(){
 
 func InitializeMembership(){
 	MEMBERSHIP_ID = strconv.Itoa(int(LogicalNode)) + strconv.Itoa(int(time.Now().Unix()))
+	TIMEOUT = 3
+	TIMEOUT_CLEANUP = 3
 	log.Printf("New Membership ID created: %s\n", MEMBERSHIP_ID);
 	SetupMembershipPort();
 	hostname, err := os.Hostname();
@@ -52,6 +60,7 @@ func MakeHeartbeat()(pb.HeartbeatMessage){
 		if ok {
 			log.Printf("The heartbeat of %s: %s\n", MEMBERSHIP_ID, entry.String());
 			entry.HeartbeatCounter = entry.HeartbeatCounter + 1;
+			entry.LocalTime = time.Now().Unix()
 			MembershipList[MEMBERSHIP_ID] = entry;
 			log.Printf("The new heartbeat for myself: %s\n", entry.String());
 		} else {
@@ -73,7 +82,6 @@ func MakeHeartbeat()(pb.HeartbeatMessage){
 		}
 		heartbeat_msg := pb.HeartbeatMessage{Table: &table, Host: host, Port: MEMBERSHIP_PORT}
 		return heartbeat_msg;
-
 }
 
 func SendHeartbeatMessage(heartbeat pb.HeartbeatMessage, address string) {
@@ -102,6 +110,7 @@ func SendHeartbeatMessage(heartbeat pb.HeartbeatMessage, address string) {
 
 func MergeTables(table pb.Table){
 	for _, entry := range(table.Entries){
+			fmt.Printf("Currently looking at this entry that I received: %s\n", entry.String());
 			log.Println("Currently looking at this entry that I received: %s\n", entry.String());
 			if entry.CurrState == pb.NodeState_DEAD {
 				// they marked the node as dead, I don't care
@@ -126,7 +135,7 @@ func MergeTables(table pb.Table){
 					log.Println("I has marked %s as dead, I don't care...\n", val.MachineId);
 					continue;
 				}
-				if(val.HeartbeatCounter > entry.HeartbeatCounter){
+				if(val.HeartbeatCounter >= entry.HeartbeatCounter){
 					log.Printf("Not updating entry for %s; I have heartbeat counter of %d while the other is %d\n", entry.MachineId, val.HeartbeatCounter, entry.HeartbeatCounter);
 					continue; // no needto update, I have a more up to date entry
 				} else {
@@ -135,7 +144,83 @@ func MergeTables(table pb.Table){
 					MembershipList[entry.MachineId] = *entry;
 					log.Println(MembershipList[entry.MachineId]);
 					log.Printf("Updated entry for %s to %s\n", entry.MachineId, entry.String());
+					fmt.Printf("Updated entry for %s to %s\n", entry.MachineId, entry.String());
 				}
 			}
 	}
 }
+
+func PruneTable() {
+	for {
+		// sleep for timeout
+		time.Sleep(time.Duration(TIMEOUT) * time.Second);
+		// acquire mutex
+		MembershipMutex.Lock();
+		for key, entry := range(MembershipList){
+			if entry.CurrState == pb.NodeState_DEAD {
+				if time.Now().Unix() - entry.MarkedDead >= TIMEOUT_CLEANUP {
+					fmt.Printf("Cleaning up %s\n", entry.MachineId);
+					log.Printf("Cleaning up %s\n", entry.MachineId);
+					// remove the entry from the list
+					delete(MembershipList, key);
+					fmt.Println("BLAH BLAH BLAH BLAH BLAH _----------------------------->\n\n\n\n");
+					log.Println("BLAH BLAH BLAH BLAH BLAH _----------------------------->\n\n\n\n");
+				}
+			} else if time.Now().Unix() - entry.LocalTime >= TIMEOUT {
+				fmt.Printf("Marked %s dead at %d\n", entry.MachineId, entry.MarkedDead);
+				entry.CurrState = pb.NodeState_DEAD;
+				entry.MarkedDead = time.Now().Unix();
+				log.Printf("Marked %s dead at %d\n", entry.MachineId, entry.MarkedDead);
+				MembershipList[key] = entry;
+			} else {
+				log.Printf("%s is safe and presumed alive\n", entry.MachineId);
+				fmt.Printf("%s is Alive because difference was only %d when time out is  %d at %d with LocalTime=%d\n", entry.MachineId, time.Now().Unix() - entry.LocalTime, TIMEOUT, time.Now().Unix(), entry.LocalTime);
+			}
+		}
+		MembershipMutex.Unlock();
+	}
+}
+
+func ListenForHeartbeats(){
+	for {
+		// read
+		buffer := make([]byte , 4096);
+		log.Println("waiting for message")
+		n, _, err := MembershipConnection.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err);
+			return
+		}
+		var received_heartbeat = pb.HeartbeatMessage{}
+		err = proto.Unmarshal(buffer[0:n], &received_heartbeat);
+		log.Printf("received a message from %s\n", received_heartbeat.Host);
+		if err != nil {
+			fmt.Println(err);
+			return;
+		}
+		// acquire mutex
+		MembershipMutex.Lock();
+		fmt.Println("The heartbeat receieved: %s\n", received_heartbeat.String());
+		MergeTables(*(received_heartbeat.Table))
+		MembershipMutex.Unlock();
+		// relinquish mutex
+	}
+}
+
+func PingHeartbeats(){
+	for {
+		time.Sleep(time.Duration(float64(TIMEOUT) / 3) * time.Second);
+		MembershipMutex.Lock();
+		for key, entry := range(MembershipList){
+			probability := rand.Float64();
+			if probability <= 3 / float64(len(MembershipList)) {
+				log.Printf("Sending heartbeat from %s to %s\n", MEMBERSHIP_ID, key);
+				new_dst := entry.Hostname + ":" + entry.Port;
+				SendHeartbeatMessage(MakeHeartbeat(), new_dst);
+			}
+		}
+		MembershipMutex.Unlock();
+	}
+}
+
+
