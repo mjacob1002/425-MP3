f504e7b12235767b41634b993c88c57353d96011
Aniket Gargya <gargya.aniket@gmail.com>
2023-11-05 05:59:37 -0600

    add store ls and delete commands

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -14,6 +14,9 @@ func ListenToCommands(){
     putRe := regexp.MustCompile(`^put (\S+) (\S+)\n$`)
     getRe := regexp.MustCompile(`^get (\S+) (\S+)\n$`)
     deleteRe := regexp.MustCompile(`^delete (\S+)\n$`)
+    lsRe := regexp.MustCompile(`^ls (\S+)\n$`)
+    storeRe := regexp.MustCompile(`^store\n$`)
+
     listMemRe := regexp.MustCompile(`^list_mem\n$`)
 
     reader := bufio.NewReader(os.Stdin)
@@ -41,7 +44,32 @@ func ListenToCommands(){
             matches := deleteRe.FindStringSubmatch(input)
             if len(matches) == 2 {
                 sdfsFilename := matches[1]
-                fmt.Printf("sdfsFilename: <%v>\n", sdfsFilename)
+                index := fs.GetFileOwner(sdfsFilename)
+                fs.Delete(fs.MachineStubs[fs.MachineIds[index]], sdfsFilename, false)
+            }
+        case lsRe.MatchString(input):
+            matches := lsRe.FindStringSubmatch(input)
+            if len(matches) == 2 {
+                sdfsFilename := matches[1]
+                index := fs.GetFileOwner(sdfsFilename)
+
+                for i := 0; i < 4; i++ {
+                    idx := (i + index) % len(fs.MachineIds)
+                    if i != 0 && idx == index {
+                        break
+                    }
+
+                    fmt.Printf("%v. %v\n", i + 1, fs.MachineIds[idx])
+                }
+            }
+        case storeRe.MatchString(input):
+            fmt.Printf("Files:\n")
+            for i, file := range fs.Files {
+                fmt.Printf("%v. %v\n", i + 1, file)
+            }
+            fmt.Printf("Replicated Files:\n")
+            for i, file := range fs.ReplicaFiles {
+                fmt.Printf("%v. %v\n", i + 1, file)
             }
         case listMemRe.MatchString(input):
             for i, machineId := range fs.MachineIds {
